c0e6473a334e4511bf6d871d1e2d53f62a4dfcfb
James Denk <jndenk26@gmail.com>
2023-04-27 20:35:19 -0500

    added js link and google search code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// Google Custom Search API
+var apiKey = "AIzaSyBYCL5vLcrfUfLD6ecGYRaDjEL8jdv0yuo";
+var apiUrl = "https://www.googleapis.com/customsearch/v1?[parameters]";
+var googleSearchBtn = document.getElementById('google-search-btn');
+
+googleSearchBtn.addEventListener('click', function() {
+// Get value of input field
+// Store value as variable
+// Pass value into function
+    googleSearchNewTab(mockInput)
+
+})
+
+var baseURL = "https://google.com/search?q=";
+var mockInput = 'How to center a div'
+function googleSearchNewTab (searchInput) {
+    var formattedInput = searchInput.replace(' ', '+')
+    window.open(baseURL + formattedInput, "_blank");
+}
+
+
 // Open Weather API
 var apiKey = "68ab3cf80e2e8d6083d2d1e1d2bb8a9a";
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
